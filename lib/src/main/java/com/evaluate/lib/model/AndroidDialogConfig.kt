05198d80059a39b53502220ea89af4a100832e89
Lonely Wasp <waspbean@gmail.com>
2020-12-17 22:11:08 +0200

    Nullable title and description

--- lib/src/main/java/com/evaluate/lib/model/AndroidDialogConfig.kt
+++ lib/src/main/java/com/evaluate/lib/model/AndroidDialogConfig.kt
@@ -3,10 +3,10 @@ package com.evaluate.lib.model
 import androidx.annotation.DrawableRes
 
 data class AndroidDialogConfig(
-        val title: String,
-        val description: String,
-        val rateBtnText: String,
-        val laterBtnText: String,
+        val title: String?,
+        val description: String?,
+        val rateBtnText: String?,
+        val laterBtnText: String?,
         val cancelIfTouchOutside: Boolean,
         val dialogCornerRadii: Float,
         @DrawableRes val icon: Int?,
